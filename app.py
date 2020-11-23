754bffe507d0bf5f95ad4e5592d8179d84ec62b2
IvanTepes <ivan.tepes@outlook.com>
2020-11-23 17:56:16 +0000

    Add Session User profile

--- app.py
+++ app.py
@@ -61,6 +61,9 @@ def register():
         # put the new user into session cookie
         session["user"] = request.form.get("username").lower()
         flash("Registration Successful!")
+
+        # if register sucessful rediret to My Recipe
+        return redirect(url_for("profile", username=session["user"]))
     return render_template("/register.html")
 
 
@@ -76,7 +79,10 @@ def login():
             if check_password_hash(
                     existing_user["password"], request.form.get("password")):
                 session["user"] = request.form.get("username").lower()
-                flash("Welcome, {}".format(request.form.get("username")))
+                flash("Welcome, {}".format(
+                    request.form.get("username")))
+                return redirect(url_for(
+                    "profile", username=session["user"]))
             else:
                 # invalid password match
                 flash("Incorrect Username and/or Password")
@@ -88,6 +94,14 @@ def login():
             return redirect(url_for("login"))
 
     return render_template("login.html")
+
+
+@app.route("/profile/<username>", methods=["GET", "POST"])
+def profile(username):
+    # grab the session user's username from db
+    username = mongo.db.users.find_one(
+        {"username": session["user"]})["username"]
+    return render_template("profile.html", username=username)
 
 
 if __name__ == "__main__":
