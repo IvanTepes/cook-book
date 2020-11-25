6df983085ab20df27809c440b45b6216b839cb5d
IvanTepes <ivan.tepes@outlook.com>
2020-11-25 16:28:13 +0000

    Added initalization for Text input, Forms select, Character counter, Format and Comment code

--- static/js/script.js
+++ static/js/script.js
@@ -1,11 +1,27 @@
 $(document).ready(function () {
+    // Nav mobile initalization
     $('.sidenav').sidenav({
         closeOnClick: true,
         draggable: true,
         edge: 'right',
     });
 
+    // Parallax initalization
     $('.parallax').parallax();
+
+    // Login and Register tabs
     $('.tabs').tabs();
+
+    // Dropdown triger for nav bar
     $(".dropdown-trigger").dropdown();
+
+    // Select dropdown initalization
+    $('select').formSelect();
+
+    // Resize window for method and integriends
+    $('#recipe-method, #recipe-ingredients').val('');
+    M.textareaAutoResize($('#recipe-method, #recipe-ingredients'));
+
+    // Character counter initalization
+    $('input#recipe-name,#recipe-cuisine,#recipe-time,#recipe-size, textarea#textarea2').characterCounter();
 });
