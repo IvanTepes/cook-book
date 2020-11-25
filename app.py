b1367f229de22475b5e5eae95d4221c0cca68245
IvanTepes <ivan.tepes@outlook.com>
2020-11-25 16:21:18 +0000

    Create view for add_recipes.html

--- app.py
+++ app.py
@@ -119,6 +119,11 @@ def profile(username):
     return redirect(url_for("login"))
 
 
+@app.route("/add_recipe")
+def add_recipe():
+    return render_template("add_recipe.html")
+
+
 @app.route("/logout")
 def logout():
     # remove user from session cookie
