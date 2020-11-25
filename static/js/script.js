236278d04b5d60b2c039b96e719b9eae9ab32187
IvanTepes <ivan.tepes@outlook.com>
2020-11-25 23:30:12 +0000

    Remove default materialize textarea attribute

--- static/js/script.js
+++ static/js/script.js
@@ -19,9 +19,9 @@ $(document).ready(function () {
     $('select').formSelect();
 
     // Resize window for method and integriends
-    $('#recipe-method, #recipe-ingredients').val('');
-    M.textareaAutoResize($('#recipe-method, #recipe-ingredients'));
+    $('#recipe_method, #recipe_ingredients').val('');
+    M.textareaAutoResize($('#recipe_method, #recipe_ingredients'));
 
     // Character counter initalization
-    $('input#recipe-name,#recipe-cuisine,#recipe-time,#recipe-size, textarea#textarea2').characterCounter();
+    $('input#recipe_name,#recipe_cuisine,#recipe_cooking_time,#recipe_size').characterCounter();
 });
