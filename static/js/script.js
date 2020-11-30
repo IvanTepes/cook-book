66f9fc2ebc3805ac085576b81ceeb270f1200d07
IvanTepes <ivan.tepes@outlook.com>
2020-11-30 05:34:01 +0000

    Remove unnecessary

--- static/js/script.js
+++ static/js/script.js
@@ -18,12 +18,8 @@ $(document).ready(function () {
     // Select dropdown initalization
     $('select').formSelect();
 
-    // Resize window for method and integriends
-    $('#recipe_method, #recipe_ingredients').val('');
-    M.textareaAutoResize($('#recipe_method, #recipe_ingredients'));
-
     // Character counter initalization
-    $('input#recipe_name,#recipe_cuisine,#recipe_cooking_time,#recipe_size,         #recipe_ingredients,#recipe_method,#recipe_image').characterCounter();
+    $('input#recipe_name,#recipe_cuisine,#recipe_cooking_time,#recipe_prep_time,         #recipe_ingredients,#recipe_method,#recipe_image').characterCounter();
 
     /* 
         Dropdown menus validation
