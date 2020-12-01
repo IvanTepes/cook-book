445c8f28b9f48bc4fe3d8fcc2c6d8f4413571f35
IvanTepes <ivan.tepes@outlook.com>
2020-12-01 21:39:31 +0000

    Add default passive event lisener

---
+++ static/js/check_page.js
@@ -0,0 +1,52 @@
+/* 
+To check page active url
+https://stackoverflow.com/questions/1034621/get-the-current-url-with-javascript
+ */
+
+/* 
+Multiple conditions 
+https://stackoverflow.com/questions/37896484/multiple-conditions-for-javascript-includes-method
+*/
+
+// Fire CheckActivePage function when page is loaded
+window.onload = function() {
+  checkActivePage();
+};
+
+/* Check where user is */
+function checkActivePage() {
+
+// Grab active window and set variable activePage
+let activePage = window.location.href;
+
+// Categories of recipes
+let categories = ["breakfast", "dinner", "lunch", "desserts"];
+
+/* Run the tests against every element in the array
+    check if user ulr contain recipe category
+    if true line >> 35
+*/
+let recipePage = categories.some(el => activePage.includes(el));
+
+
+if (activePage == gitPage ) {
+  document.getElementById("home").classList.toggle("visited");
+} else if (recipePage == true) {
+  document.getElementById("recipe").classList.toggle("visited");
+} else if (activePage.includes("index")) {
+  document.getElementById("home").classList.toggle("visited");
+} else if (activePage.includes("login")) {
+  document.getElementById("login").classList.toggle("visited");
+} else if (activePage.includes("register")) {
+  document.getElementById("register").classList.toggle("visited");
+} else if (activePage.includes("my_recipes")) {
+  document.getElementById("myRecipes").classList.toggle("visited");
+} else if (activePage.includes("add_recipe")) {
+  document.getElementById("addRecipe").classList.toggle("visited");
+} else if (activePage.includes("edit_recipe")) {
+  document.getElementById("myRecipes").classList.toggle("visited");
+} else { (activePage.includes("logout")) 
+  document.getElementById("logout").classList.toggle("visited");
+}
+
+}
